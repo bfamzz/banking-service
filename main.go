180fd7ef66a6ed9300145905ab80e8a588108fdb
Busayo Famutimi <famutimi.busayo@gmail.com>
2023-03-06 17:05:48 +0100

    added swagger front-end files and impl file server (#13)

--- main.go
+++ main.go
@@ -86,6 +86,9 @@ func rungRPCGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
+	fs := http.FileServer(http.Dir("./docs/swagger"))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot create grpc listener:", err)
