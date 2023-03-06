7bf69a55577a6eb4d75d19ad0748cbd86cfae51e
Busayo Famutimi <famutimi.busayo@gmail.com>
2023-03-06 14:48:42 +0100

    added formatting directive to error output

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -29,7 +29,7 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		if pqErr, ok := err.(*pq.Error); ok {
 			switch pqErr.Code.Name() {
 			case "unique_violation":
-				return nil, status.Errorf(codes.AlreadyExists, "username already exists", err)
+				return nil, status.Errorf(codes.AlreadyExists, "username already exists: %s", err)
 			}
 		}
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
